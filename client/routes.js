8aefef3948a75067803fdcb1fd23734a7b3a8c68
Amantina Sosa <amantinasosa@gmail.com>
2020-08-26 08:01:23 -0400

    routes updated

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, Search} from './components'
+import {Login, Signup, UserHome, Search, Shoppies} from './components'
 import {me} from './store'
 
 /**
@@ -21,7 +21,7 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route path="/search" component={Search} />
+        <Route path="/search" component={Shoppies} />
 
         {isLoggedIn && (
           <Switch>
